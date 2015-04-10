7e839d695e1a5d00b9b4af9296e3a55ea3a24d64
peakmuma <peakmuma@163.com>
2015-04-10 11:15:55 +0800

    use list save the url,title,commentNum

--- getClUrl.py
+++ getClUrl.py
@@ -1,24 +1,29 @@
 import urllib.request
 from bs4 import BeautifulSoup
+from operator import itemgetter
 
 external_url='http://gfw74.info/thread0806.php?fid=22&search=&page={0}'
+external_list=[]
 
 def getUrl():
-	for i in range(1):
+	for i in range(8):
 		pageUrl=external_url.format(str(i+2))
 		pageSoup=getSoup(pageUrl)
 		if pageSoup is not None:
 			trSoups=pageSoup.findAll('tr',class_='tr3 t_one')
 			for trSoup in trSoups:
 				processTrSoup(trSoup)
+	global external_list
+	external_list=sorted(external_list,key=itemgetter('commentNum'),reverse=True)
+	print(external_list[:10])
 
 def processTrSoup(trSoup):
 	tdSoups=trSoup.contents
 	aSoup=tdSoups[1].find('h3').a
 	commentNum=tdSoups[5].string
-	print(aSoup.get('href'))
-	print(str(aSoup.string,encoding='gbk'))
-	print(commentNum)
+	oneRecord={'url':aSoup.get('href'),'title':aSoup.string,'commentNum':int(commentNum)}
+	global external_list
+	external_list.append(oneRecord)
 
 def getSoup(url,reConTimes=3):
 	for i in range(reConTimes):#todo,need i?
